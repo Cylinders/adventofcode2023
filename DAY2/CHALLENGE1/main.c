9fa7f6ab1370ce112507a3bd0e9fee06dbe068ea
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-12-04 08:20:25 -0500

    Some changes to Day 1 Challenge 1

--- DAY2/CHALLENGE1/main.c
+++ DAY2/CHALLENGE1/main.c
@@ -1,90 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
+#include <String>
+int main()
+{
     FILE *file;
-    char line[100];  // Adjust the size as needed
-    int len = 0; 
-    int * lenp = &len; 
-
-    printf("hey");
-    return 0;   
+    char line[100]; // Adjust the size as needed
+    int len = 0;
+    int *lenp = &len;
 
     // Open the file for reading
     file = fopen("input.txt", "r");
 
     // Check if the file opened successfully
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Error opening file");
         return 1; // Exit with an error code
     }
 
     // Read and print each line in the file
-    while (fgets(line, sizeof(line), file) != NULL) {
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        int currentSum[3] = [ 0, 0, 0 ];
         len = strlen(line);
-        for (int i = 0; i < len; ++i) {
-            // This section will check if the thing is a integer or not. 
-            char x = line[0];
-            char * character = &x;
-            int isIt; 
-            int * is = &isIt; 
-            switch (x) {
-                case '0':
-                    isIt = 0; 
-                    break;
-                case '1':
-                    isIt = 1; 
-                    break;
-                case '2':
-                    isIt = 2; 
-                    break;
-                case '3':
-                    isIt = 3; 
-                    break;
-                case '4':
-                    isIt = 4; 
-                    break;
-                case '5':
-                    isIt = 5; 
-                    break;
-                case '6':
-                    isIt = 6; 
-                    break;
-                case '7':
-                    isIt = 7; 
-                    break;
-                case '8':
-                    isIt = 8; 
-                    break;
-                case '9':
-                    isIt = 9; 
-                    break;
-                default: 
-                    free(is);
-            }
+        char currentWord[3];
+        for (int i = 0; i < len; ++i)
+        {
+            // This section will check if the thing is a integer or not.
+            char x = line[i];
+            char *xP = &x;
+            int y = x;
+            free(xP);
 
-            if (free != NULL) {
-                if (i < len - 1) {
-                    
+            if (x <= 39 && x >= 30)
+            {
+
+                printf("This character represents an integer \n");
+                strcat(currentWord, x);
+            }
+            if (strcom(line[i + 1], ""))
+            {
+                switch (line[i + 2])
+                {
+                case 'r':
+                    ++currentSum[0];
+                    break;
+                case 'g':
+                    ++currentSum[1];
+                    break;
+                case 'b':
+                    ++currentSum[2];
+                    break;
                 }
             }
         }
     }
 
     free(lenp);
-    
-    
 
     // Close the file
     fclose(file);
 
     return 0; // Exit successfully
-}
-
-int returnCharNum(int * x){
-
-    switch (*x){
-        
-    }
-
 }
