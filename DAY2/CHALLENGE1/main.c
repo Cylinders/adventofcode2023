b485dc958add96e38cdc0616dc8de18b21825588
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-12-03 21:11:01 -0500

    C is much more difficult and the fact that I am doing it here justifies not being able to finish.

--- DAY2/CHALLENGE1/main.c
+++ DAY2/CHALLENGE1/main.c
@@ -7,6 +7,8 @@ int main() {
     int len = 0; 
     int * lenp = &len; 
 
+    printf("hey");
+    return 0;   
 
     // Open the file for reading
     file = fopen("input.txt", "r");
@@ -21,14 +23,68 @@ int main() {
     while (fgets(line, sizeof(line), file) != NULL) {
         len = strlen(line);
         for (int i = 0; i < len; ++i) {
+            // This section will check if the thing is a integer or not. 
+            char x = line[0];
+            char * character = &x;
+            int isIt; 
+            int * is = &isIt; 
+            switch (x) {
+                case '0':
+                    isIt = 0; 
+                    break;
+                case '1':
+                    isIt = 1; 
+                    break;
+                case '2':
+                    isIt = 2; 
+                    break;
+                case '3':
+                    isIt = 3; 
+                    break;
+                case '4':
+                    isIt = 4; 
+                    break;
+                case '5':
+                    isIt = 5; 
+                    break;
+                case '6':
+                    isIt = 6; 
+                    break;
+                case '7':
+                    isIt = 7; 
+                    break;
+                case '8':
+                    isIt = 8; 
+                    break;
+                case '9':
+                    isIt = 9; 
+                    break;
+                default: 
+                    free(is);
+            }
 
+            if (free != NULL) {
+                if (i < len - 1) {
+                    
+                }
+            }
         }
     }
 
-
+    free(lenp);
+    
+    
 
     // Close the file
     fclose(file);
 
     return 0; // Exit successfully
+}
+
+int returnCharNum(int * x){
+
+    switch (*x){
+        
+    }
+
 }
